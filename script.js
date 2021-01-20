8f0e520670a0394600e8d054a5eb03dd5aa94ca8
Driscollart <Patrick@driscollart.com>
2021-01-19 20:26:48 -0500

    initial upload of chart
    finished chart

---
+++ script.js
@@ -0,0 +1,441 @@
+var alphabet = [];
+
+alphabet.push({
+    letter: "a",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "b",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "c",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "d",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "e",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "f",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "g",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "h",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "i",
+    cell1: false,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "j",
+    cell1: false,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "k",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "l",
+    cell1: true,
+    cell2: true,
+    cell3: true,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "m",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "n",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "o",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "p",
+    cell1: true,
+    cell2: true,
+    cell3: true,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "q",
+    cell1: true,
+    cell2: true,
+    cell3: true,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "r",
+    cell1: true,
+    cell2: true,
+    cell3: true,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "s",
+    cell1: false,
+    cell2: true,
+    cell3: true,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+alphabet.push({
+    letter: "t",
+    cell1: false,
+    cell2: true,
+    cell3: true,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+alphabet.push({
+    letter: "u",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: false,
+    cell5: false,
+    cell6: true
+});
+alphabet.push({
+    letter: "v",
+    cell1: true,
+    cell2: true,
+    cell3: true,
+    cell4: false,
+    cell5: false,
+    cell6: true
+});
+alphabet.push({
+    letter: "w",
+    cell1: false,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: true,
+    cell6: true
+});
+alphabet.push({
+    letter: "x",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: true,
+    cell5: false,
+    cell6: true
+});
+alphabet.push({
+    letter: "y",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: true,
+    cell5: true,
+    cell6: true
+});
+alphabet.push({
+    letter: "z",
+    cell1: true,
+    cell2: false,
+    cell3: true,
+    cell4: false,
+    cell5: true,
+    cell6: true
+});
+
+var numbers = [];
+
+numbers.push({
+    letter: "Num Sign",
+    cell1: false,
+    cell2: false,
+    cell3: true,
+    cell4: true,
+    cell5: true,
+    cell6: true
+});
+numbers.push({
+    letter: "1",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+numbers.push({
+    letter: "2",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+numbers.push({
+    letter: "3",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+numbers.push({
+    letter: "4",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+numbers.push({
+    letter: "5",
+    cell1: true,
+    cell2: false,
+    cell3: false,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+numbers.push({
+    letter: "6",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+numbers.push({
+    letter: "7",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: true,
+    cell6: false
+});
+numbers.push({
+    letter: "8",
+    cell1: true,
+    cell2: true,
+    cell3: false,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+numbers.push({
+    letter: "9",
+    cell1: false,
+    cell2: true,
+    cell3: false,
+    cell4: true,
+    cell5: false,
+    cell6: false
+});
+numbers.push({
+    letter: "0",
+    cell1: false,
+    cell2: false,
+    cell3: true,
+    cell4: true,
+    cell5: true,
+    cell6: true
+});
+
+var punctuation = [];
+
+punctuation.push({
+    letter: "Capital",
+    cell1: false,
+    cell2: false,
+    cell3: false,
+    cell4: false,
+    cell5: false,
+    cell6: true
+});
+punctuation.push({
+    letter: "Period",
+    cell1: false,
+    cell2: true,
+    cell3: false,
+    cell4: false,
+    cell5: true,
+    cell6: true
+});
+punctuation.push({
+    letter: "?",
+    cell1: false,
+    cell2: true,
+    cell3: true,
+    cell4: false,
+    cell5: false,
+    cell6: true
+});
+punctuation.push({
+    letter: "!",
+    cell1: false,
+    cell2: true,
+    cell3: true,
+    cell4: false,
+    cell5: true,
+    cell6: false
+});
+punctuation.push({
+    letter: "Comma",
+    cell1: false,
+    cell2: true,
+    cell3: false,
+    cell4: false,
+    cell5: false,
+    cell6: false
+});
+
+function ViewModel() {
+    var self = this;
+    self.alphabet = alphabet;
+    self.numbers = numbers;
+    self.punctuation = punctuation;
+
+    self.getCell = function (cell) {
+        if (cell === true) {
+            return "•"
+        } else {
+            return "◦";
+        }
+    }
+
+    self.getNumbers = function (index, type) {
+        var numbers = [];
+        var currentItem = null;
+        switch (type) {
+            case 'alphabet':
+                currentItem = self.alphabet[index];
+                break;
+            case 'numbers':
+                currentItem = self.numbers[index];
+                break;
+            case 'punctuation':
+                currentItem = self.punctuation[index];
+                break;
+        }
+
+        if (currentItem.cell1 === true) {
+            numbers.push('1');
+        }
+        if (currentItem.cell2 === true) {
+            numbers.push('2');
+        }
+        if (currentItem.cell3 === true) {
+            numbers.push('3');
+        }
+        if (currentItem.cell4 === true) {
+            numbers.push('4');
+        }
+        if (currentItem.cell5 === true) {
+            numbers.push('5');
+        }
+
+        if (currentItem.cell6 === true) {
+            numbers.push('6');
+        }
+
+        return numbers.join(', ');
+    }
+};
+
+ko.applyBindings(new ViewModel());
